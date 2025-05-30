be0b556b97e842eb7e9af6298338377cb101cad9
Bardiya <bardiya.basafa@gmail.com>
2025-05-30 22:27:08 +0330

    ops: add required nuget packages for identity

---
+++ Golestan.Domain/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+﻿namespace Golestan.Domain.Entities;
+
+using Enums;
+using Microsoft.AspNetCore.Identity;
+
+
+public class AppUser : IdentityUser {
+
+    // Common properties for all users
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string NationalId { get; set; }
+
+    public UserType UserType { get; set; }// Discriminator property
+
+}

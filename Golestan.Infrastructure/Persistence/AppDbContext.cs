1a58559cbb83f13513f67bc7eb1ee0f221bfca5f
Bardiya <bardiya.basafa@gmail.com>
2025-05-30 23:20:02 +0330

    ops: add app roles

--- Golestan.Infrastructure/Persistence/AppDbContext.cs
+++ Golestan.Infrastructure/Persistence/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿namespace Golestan.Infrastructure.Persistence;
 
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
-public class AppDbContext : DbContext {
+public class AppDbContext : IdentityDbContext<AppUser> {
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
---
+++ Golestan.Shared/Constants/AppRoles.cs
@@ -0,0 +1,11 @@
+﻿namespace Golestan.Shared.Constants;
+
+public static class AppRoles {
+
+    public const string Admin = "Admin";
+
+    public const string User = "Student";
+
+    public const string Instructor = "Instructor";
+
+}

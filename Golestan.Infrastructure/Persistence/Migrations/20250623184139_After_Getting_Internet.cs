7a634c02867fff9ec8534407cc3f59c0fc81e531
Bardiya <bardiya.basafa@gmail.com>
2025-07-03 17:04:47 +0330

    push remains of students-cotroller

---
+++ Golestan.Infrastructure/Persistence/Migrations/20250623184139_After_Getting_Internet.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Golestan.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class After_Getting_Internet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "14c9dcae-da6a-4833-af53-7dbda657612b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "773b09da-08f1-425a-a3a1-bae5ea00aad8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d3e9e74d-5d7d-4ed5-bfe3-e2310181fe68");
+
+            migrationBuilder.DropColumn(
+                name: "ExamTime",
+                table: "Courses");
+
+            migrationBuilder.RenameColumn(
+                name: "MajorName",
+                table: "Faculties",
+                newName: "Major");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TermId",
+                table: "Sections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExamId",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PrerequisiteCourses",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "Courses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Terms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExamsStartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExamsEndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SectionSelectionStartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SectionSelectionEndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TermNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsFirstTerm = table.Column<bool>(type: "bit", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    ExamSuspended = table.Column<bool>(type: "bit", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Terms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Terms_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExamResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Score = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Objection = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExamDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TermId = table.Column<int>(type: "int", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    InstructorId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    SectionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExamResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExamResults_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ExamResults_Instructors_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "Instructors",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ExamResults_Sections_SectionId",
+                        column: x => x.SectionId,
+                        principalTable: "Sections",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ExamResults_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ExamResults_Terms_TermId",
+                        column: x => x.TermId,
+                        principalTable: "Terms",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TimeSlot = table.Column<int>(type: "int", nullable: false),
+                    ExamDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    ClassroomId = table.Column<int>(type: "int", nullable: false),
+                    TermId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exams", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Exams_Classrooms_ClassroomId",
+                        column: x => x.ClassroomId,
+                        principalTable: "Classrooms",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Exams_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Exams_Terms_TermId",
+                        column: x => x.TermId,
+                        principalTable: "Terms",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "27a8a935-288c-4aa1-a42e-93a0212ecbda", null, "Student", "STUDENT" },
+                    { "91f455b1-0956-4c1b-a43b-774596aa309f", null, "Admin", "ADMIN" },
+                    { "be781783-ee49-407f-86fd-d42c4afc2b38", null, "Instructor", "INSTRUCTOR" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_TermId",
+                table: "Sections",
+                column: "TermId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_StudentId",
+                table: "Courses",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExamResults_CourseId",
+                table: "ExamResults",
+                column: "CourseId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExamResults_InstructorId",
+                table: "ExamResults",
+                column: "InstructorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExamResults_SectionId",
+                table: "ExamResults",
+                column: "SectionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExamResults_StudentId",
+                table: "ExamResults",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExamResults_TermId",
+                table: "ExamResults",
+                column: "TermId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_ClassroomId",
+                table: "Exams",
+                column: "ClassroomId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_CourseId",
+                table: "Exams",
+                column: "CourseId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_TermId",
+                table: "Exams",
+                column: "TermId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Terms_StudentId",
+                table: "Terms",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Students_StudentId",
+                table: "Courses",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sections_Terms_TermId",
+                table: "Sections",
+                column: "TermId",
+                principalTable: "Terms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Students_StudentId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sections_Terms_TermId",
+                table: "Sections");
+
+            migrationBuilder.DropTable(
+                name: "ExamResults");
+
+            migrationBuilder.DropTable(
+                name: "Exams");
+
+            migrationBuilder.DropTable(
+                name: "Terms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_TermId",
+                table: "Sections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_StudentId",
+                table: "Courses");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "27a8a935-288c-4aa1-a42e-93a0212ecbda");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "91f455b1-0956-4c1b-a43b-774596aa309f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "be781783-ee49-407f-86fd-d42c4afc2b38");
+
+            migrationBuilder.DropColumn(
+                name: "TermId",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "ExamId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "PrerequisiteCourses",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Courses");
+
+            migrationBuilder.RenameColumn(
+                name: "Major",
+                table: "Faculties",
+                newName: "MajorName");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExamTime",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "14c9dcae-da6a-4833-af53-7dbda657612b", null, "Instructor", "INSTRUCTOR" },
+                    { "773b09da-08f1-425a-a3a1-bae5ea00aad8", null, "Student", "STUDENT" },
+                    { "d3e9e74d-5d7d-4ed5-bfe3-e2310181fe68", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}

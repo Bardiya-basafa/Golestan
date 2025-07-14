e9dc922e7add7f738dc08acb14577d5609637bdf
Bardiya <bardiya.basafa@gmail.com>
2025-07-14 10:51:52 +0330

    new selection branch

--- Golestan.Application/obj/Release/net9.0/Golestan.Application.AssemblyInfo.cs
+++ Golestan.Application/obj/Release/net9.0/Golestan.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Golestan.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f598d775bb7deddf71641669b149b36db8ebf1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74ecfbd9be1051b7ec5f1abf9b490edcb094177")]
 [assembly: System.Reflection.AssemblyProductAttribute("Golestan.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Golestan.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

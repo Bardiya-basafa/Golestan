8630257de84649de53c51f2ace5f2d1bd13738d1
Bardiya <bardiya.basafa@gmail.com>
2025-07-21 09:48:51 +0330

    remove npm files

--- Golestan.Domain/obj/Release/net9.0/Golestan.Domain.AssemblyInfo.cs
+++ Golestan.Domain/obj/Release/net9.0/Golestan.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Golestan.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b7c754802683bac5b9775256b99098ecd2c863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30e7883f2feea4a733a4dc78b68943bbc29b6e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Golestan.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Golestan.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Golestan.Web/obj/Release/net9.0/Golestan.Web.AssemblyInfo.cs
+++ Golestan.Web/obj/Release/net9.0/Golestan.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Golestan.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6966713a344b6d0e2b8ea29c66b2fc81a0bcf622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30e7883f2feea4a733a4dc78b68943bbc29b6e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Golestan.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Golestan.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
